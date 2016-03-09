bf14e26c5e239b99c82a0fb0d353187fdeaf0fb0
高艾寒 <gaoaihan_lily@sina.com>
2016-03-09 19:48:10 +0800

    xiugai9

--- canvas_oop/canvas_oop.ts
+++ canvas_oop/canvas_oop.ts
@@ -111,25 +111,25 @@ rect.color = '#04a3f4'
 
 
 var rect2 = new Rect();
-rect2.width = 100;
-rect2.height = 50;
-rect2.x = 360;
-rect2.y = 250;
+rect2.width = 120;
+rect2.height = 40;
+rect2.x = 380;
+rect2.y = 220;
 //rect2.rotation = Math.PI / 8;
 rect2.color = '#95de4e'
 
 var rect3 = new Rect();
-rect3.width = 100;
-rect3.height = 50;
-rect3.x = 210;
-rect3.y = 250;
+rect3.width = 120;
+rect3.height = 40;
+rect3.x = 220;
+rect3.y = 220;
 rect3.color = '#086fbe'
 
 var rect4 = new Rect();
-rect4.width = 100;
-rect4.height = 50;
-rect4.x = 510;
-rect4.y = 250;
+rect4.width = 120;
+rect4.height = 40;
+rect4.x = 530;
+rect4.y = 220;
 rect4.color = '#086fbe'
 
 
